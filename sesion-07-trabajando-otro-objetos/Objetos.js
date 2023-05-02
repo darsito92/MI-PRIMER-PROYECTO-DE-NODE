fc1a2d85cd45f90756c2875e4458236db7ce1b6a
Diego Sepulveda <diego.sepulvedat92@gmail.com>
2023-05-02 00:18:16 -0400

    Mensaje de confirmación

---
+++ sesion-07-trabajando-otro-objetos/Objetos.js
@@ -0,0 +1,26 @@
+const persona = {
+    nombre:"diego",
+    apellido:"sepulveda",
+    edad:30,
+    altura:168,
+    eresDesarrollador:true
+}
+
+const edad = persona.edad
+console.log(edad)
+const lista = [{...persona},
+    {nombre:"lesie",
+    apellido:"silva",
+    edad:29,
+    altura:155,
+    eresDesarrollador:false},
+    {nombre:"raul",
+    apellido:"santillan",
+    edad:31,
+    altura:183,
+    eresDesarrollador:false}]
+
+
+const newLista = lista.sort((a,b)=> b.edad-a.edad)
+console.log(newLista)
+
